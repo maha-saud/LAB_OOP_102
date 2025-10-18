41f249e7c3844389223ef8301d6b325fe1bc3d10
Maha Saud <al-maha2244@hotmail.com>
2025-10-18 19:08:30 +0300

    modify

--- bank_account.py
+++ bank_account.py
@@ -20,18 +20,31 @@ class BankAccount:
 
     
     def get_account_holder(self):
+        '''this method returns the name of the account holder.'''
+
         return f"The name of the account holder: {self.account_holder}"
     
     def deposit(self, amount:float):
+        '''
+        this method accepts an amount and adds it to the account balance, 
+        and then returns the amount has been deposited & updated balance.
+        '''
         self.__balance += amount
         return f"The amount has been deposited is {amount} and your current balance is {self.__balance}"
     
-    def dragging(self, amount:float):
+    def withdraw(self, amount:float):
+        '''
+        this method accepts an amount and subtracts it from the account balance, 
+        returning an amountthe that subtracted & the updated balance, 
+        but it this only happens when if there are sufficient funds in the account.
+        If there are insufficient funds, it will raise an exception and leave the balance unchanged
+        '''
         if self.__balance >= amount:
             self.__balance -= amount
             return f"An amount has been withdrawn is {amount} from your account and your current balance is {self.__balance}"
         else:
             raise InsufficientFundsError(f"Insufficient Funds: balance is {self.__balance}, tried to withdraw {amount}")
-        
+
+# This class inherits from the built-in Exception class        
 class InsufficientFundsError(Exception):
     pass
